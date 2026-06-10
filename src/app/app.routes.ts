d6cceaa53dec19f3c06d1ca186f1529f66877572
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-10 12:52:19 -0300

    i add public routes to app.routes.ts file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  {
+      path: 'public',
+      loadChildren: () => import('./public/public.routes').then(m => m.PUBLIC_ROUTES),
+  },
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) }
 ];
