640a0960d163dd8f7ff7b4ca2d191e89fac16666
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-10 20:11:52 -0300

    i add methods singup and login to users service

--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  public singup(user: any): Observable<any> {
+    let params = JSON.stringify(user);
+    let headers = new HttpHeaders().set('content-type','application/json');
+
+    return this._http.post(environment.apiUrl + 'register', params, {headers:headers});
+  }
+
+  public login(user: any, gettoken: string | null = null): Observable<any> {
+    if (gettoken != null) {
+      user.gettoken = gettoken;
+    }
+
+    let params = JSON.stringify(user);
+    let headers = new HttpHeaders().set('content-type', 'application/json');
+
+    return this._http.post(environment.apiUrl + 'login', params, { headers: headers });
+  }
 }
