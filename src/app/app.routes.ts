3f1cfef5f2a5e21d5f3b1c7d56704424de96080a
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-10 15:28:25 -0300

    i add ** path to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,6 @@ export const routes: Routes = [
       loadChildren: () => import('./public/public.routes').then(m => m.PUBLIC_ROUTES),
   },
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) }
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) },
+  { path: '**', redirectTo: 'public/home' }
 ];
