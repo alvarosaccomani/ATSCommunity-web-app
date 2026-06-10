4dd671352f5466bffbcc5c33bab631d49da52045
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-10 20:04:39 -0300

    i add the files environment.ts and environment.prod.ts to project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.atscommunity.com.ar/api/',
+  apiUrlSocket: 'https://api.atscommunity.com.ar',
+  appName: 'ATS Community'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3006/api/',
+  apiUrlSocket: 'http://localhost:3006',
+  appName: 'ATS Community (Dev)'
+};
