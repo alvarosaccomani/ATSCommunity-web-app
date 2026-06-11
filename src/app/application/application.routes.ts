ad5b77eb34267da3aa3ff5430558bd8b1fb4830c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-11 09:11:39 -0300

    i add units route to application.routes.ts

--- src/app/application/application.routes.ts
+++ src/app/application/application.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 
 import { ApplicationLayoutComponent } from './application-layout/application-layout.component';
+import { UnitsComponent } from './units/units.component';
 
 export const APPLICATION_ROUTES: Routes = [
     {
         path: '',
         component: ApplicationLayoutComponent,
-        children: []
+        children: [
+            { path: 'units', component: UnitsComponent},
+        ]
     }
 ];
