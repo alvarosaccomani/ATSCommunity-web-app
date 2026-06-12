6f63fbe00ee02112f0b74edf68b53a45771ead17
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-12 13:52:57 -0300

    i add my-claims component to project

---
+++ src/app/user/my-claims/my-claims.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-claims',
+  imports: [],
+  templateUrl: './my-claims.component.html',
+  styleUrl: './my-claims.component.scss'
+})
+export class MyClaimsComponent {
+
+}
