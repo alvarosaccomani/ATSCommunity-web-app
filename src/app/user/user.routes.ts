1a0b0e293542b01f7931d3237249af97f85102e3
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-12 20:26:12 -0300

    i add my-transactions route to user.routes.ts

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -4,6 +4,7 @@ import { UserLayoutComponent } from './user-layout/user-layout.component';
 import { MyUnitsComponent } from './my-units/my-units.component';
 import { MyClaimsComponent } from './my-claims/my-claims.component';
 import { MyFeesComponent } from './my-fees/my-fees.component';
+import { MyTransactionsComponent } from './my-transactions/my-transactions.component';
 
 export const USER_ROUTES: Routes = [
     {
@@ -12,7 +13,8 @@ export const USER_ROUTES: Routes = [
         children: [
             { path: 'my-units', component: MyUnitsComponent},
             { path: 'my-claims', component: MyClaimsComponent},
-            { path: 'my-fees', component: MyFeesComponent}
+            { path: 'my-fees', component: MyFeesComponent},
+            { path: 'my-transactions', component: MyTransactionsComponent}
         ]
     }
 ];
