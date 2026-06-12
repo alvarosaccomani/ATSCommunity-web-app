16e8661c1718e0c0e6fcd428f6a612287b1c71be
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-12 13:53:16 -0300

    i add my-claims route to user.routes.ts

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { UserLayoutComponent } from './user-layout/user-layout.component';
 import { MyUnitsComponent } from './my-units/my-units.component';
+import { MyClaimsComponent } from './my-claims/my-claims.component';
 
 export const USER_ROUTES: Routes = [
     {
@@ -9,6 +10,7 @@ export const USER_ROUTES: Routes = [
         component: UserLayoutComponent,
         children: [
             { path: 'my-units', component: MyUnitsComponent},
+            { path: 'my-claims', component: MyClaimsComponent}
         ]
     }
 ];
