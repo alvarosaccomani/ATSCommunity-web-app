43f5f731dbcd68ed6adfc8682b6789d271eb7bd0
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-11 23:41:52 -0300

    i add my-units route to user.routes.ts

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 
 import { UserLayoutComponent } from './user-layout/user-layout.component';
+import { MyUnitsComponent } from './my-units/my-units.component';
 
 export const USER_ROUTES: Routes = [
     {
         path: '',
         component: UserLayoutComponent,
-        children: []
+        children: [
+            { path: 'my-units', component: MyUnitsComponent},
+        ]
     }
 ];
