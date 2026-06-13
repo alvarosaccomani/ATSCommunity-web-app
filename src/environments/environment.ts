0578ebf5bf61ec301eeeda89693125ddf8814b3d
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-13 19:31:14 -0300

    i change port 3006 by 3004 in environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3006/api/',
-  apiUrlSocket: 'http://localhost:3006',
+  apiUrl: 'http://localhost:3004/api/',
+  apiUrlSocket: 'http://localhost:3004',
   appName: 'ATS Community (Dev)'
 };
