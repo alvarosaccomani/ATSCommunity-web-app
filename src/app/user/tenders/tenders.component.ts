1fde72a69aa94a1d1478147508bc3fbe3730c686
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-13 10:33:06 -0300

    i add tenders component to project

---
+++ src/app/user/tenders/tenders.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tenders',
+  imports: [],
+  templateUrl: './tenders.component.html',
+  styleUrl: './tenders.component.scss'
+})
+export class TendersComponent {
+
+}
