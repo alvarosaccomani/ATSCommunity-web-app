71364f5a041232d1493eb9b7b73ab4299a368661
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-13 09:24:16 -0300

    i add BellOutline, BellFill, NotificationOutline, InfoCircleOutline, ExclamationCircleOutline, CloseCircleOutline, CheckCircleOutline, DeleteOutline, EyeOutline, EyeInvisibleOutline, MessageOutline to icons-provider

--- src/app/icons-provider.ts
+++ src/app/icons-provider.ts
@@ -2,7 +2,35 @@ import {
   MenuFoldOutline,
   MenuUnfoldOutline,
   FormOutline,
-  DashboardOutline
+  DashboardOutline,
+  BellOutline,
+  BellFill,
+  NotificationOutline,
+  InfoCircleOutline,
+  ExclamationCircleOutline,
+  CloseCircleOutline,
+  CheckCircleOutline,
+  DeleteOutline,
+  EyeOutline,
+  EyeInvisibleOutline,
+  MessageOutline
 } from '@ant-design/icons-angular/icons';
 
-export const icons = [MenuFoldOutline, MenuUnfoldOutline, DashboardOutline, FormOutline];
+export const icons = [
+  MenuFoldOutline, 
+  MenuUnfoldOutline, 
+  DashboardOutline, 
+  FormOutline,
+  BellOutline,
+  BellFill,
+  NotificationOutline,
+  InfoCircleOutline,
+  ExclamationCircleOutline,
+  CloseCircleOutline,
+  CheckCircleOutline,
+  DeleteOutline,
+  EyeOutline,
+  EyeInvisibleOutline,
+  MessageOutline
+];
+
