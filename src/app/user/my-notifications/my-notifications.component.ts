89ff72e4c66a52ff13b5944b0d726afbdd952d77
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-13 08:56:02 -0300

    i add my-notifications component to project

---
+++ src/app/user/my-notifications/my-notifications.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-notifications',
+  imports: [],
+  templateUrl: './my-notifications.component.html',
+  styleUrl: './my-notifications.component.scss'
+})
+export class MyNotificationsComponent {
+
+}
