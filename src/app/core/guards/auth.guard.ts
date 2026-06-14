a3ad602ad06db35e9d9738a34e5ee702ca362958
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-13 23:15:26 -0300

    i add code to auth.guard.ts

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,5 +1,14 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { SessionService } from '../services/session.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const sessionService = inject(SessionService);
+  const router = inject(Router);
+
+  if (sessionService.getIdentity()) {
+    return true;
+  }
+
+  return router.parseUrl('/auth/login');
 };
