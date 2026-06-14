2a18268fc2f66518c4edb204a555c03ba663fc82
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-13 23:15:46 -0300

    i add code to guest.guard.ts

--- src/app/core/guards/guest.guard.ts
+++ src/app/core/guards/guest.guard.ts
@@ -1,5 +1,14 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { SessionService } from '../services/session.service';
 
 export const guestGuard: CanActivateFn = (route, state) => {
+  const sessionService = inject(SessionService);
+  const router = inject(Router);
+
+  if (sessionService.getIdentity()) {
+    return router.parseUrl('/welcome');
+  }
+
   return true;
 };
