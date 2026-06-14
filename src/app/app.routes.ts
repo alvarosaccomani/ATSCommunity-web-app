bd81ec223e30a1b7542a179457b2456a53b1920c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-13 23:16:26 -0300

    i add guestGuard, authGuard and adminGuard to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
+import { adminGuard } from './core/guards/admin.guard';
+import { guestGuard } from './core/guards/guest.guard';
 
 export const routes: Routes = [
   {
       path: 'auth',
+      canActivate: [guestGuard],
       loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES),
   },
   {
@@ -11,13 +15,15 @@ export const routes: Routes = [
   },
   {
       path: 'user',
+      canActivate: [authGuard],
       loadChildren: () => import('./user/user.routes').then(m => m.USER_ROUTES),
   },
   {
       path: 'application',
+      canActivate: [adminGuard],
       loadChildren: () => import('./application/application.routes').then(m => m.APPLICATION_ROUTES),
   },
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) },
+  { path: 'welcome', canActivate: [authGuard], loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) },
   { path: '**', redirectTo: 'public/home' }
 ];
